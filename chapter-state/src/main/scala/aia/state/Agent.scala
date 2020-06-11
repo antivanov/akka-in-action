13269ddcd19994df752d8a2edf39d6012d789f0d
Anton Ivanov <anton.al.ivanov@gmail.com>
2020-06-12 01:20:29 +0300

    Re-factor duplication, avoid using Await.result not in test

--- chapter-state/src/main/scala/aia/state/Agent.scala
+++ chapter-state/src/main/scala/aia/state/Agent.scala
@@ -2,48 +2,37 @@ package aia.state
 
 import akka.agent.Agent
 import akka.actor.ActorSystem
-import concurrent.Await
+
+import concurrent.{Await, Future}
 import concurrent.duration._
 import akka.util.Timeout
-//import concurrent.ExecutionContext.Implicits.global
 
 
-case class BookStatistics(val nameBook: String, nrSold: Int)
-case class StateBookStatistics(val sequence: Long,
-                            books: Map[String, BookStatistics])
+case class BookStatistics(book: String, nrSold: Int)
+case class StateBookStatistics(sequence: Long, books: Map[String, BookStatistics])
 
 
 class BookStatisticsMgr(system: ActorSystem) {
   implicit val ex = system.dispatcher //todo: change chapter 2.2 =>2.3
-  val stateAgent = Agent(new StateBookStatistics(0, Map())) //todo: change chapter 2.2 =>2.3
+  val stateAgent = Agent(StateBookStatistics(0, Map())) //todo: change chapter 2.2 =>2.3
 
-  def addBooksSold(book: String, nrSold: Int): Unit = {
-    stateAgent send (oldState => {
-      val bookStat = oldState.books.get(book) match {
-        case Some(bookState) =>
-          bookState.copy(nrSold = bookState.nrSold + nrSold)
-        case None => new BookStatistics(book, nrSold)
-      }
-      oldState.copy(oldState.sequence + 1,
-        oldState.books + (book -> bookStat))
-    })
+  private def updateBooksSold(book: String, nrSold: Int)(oldState: StateBookStatistics): StateBookStatistics = {
+    val bookStat = oldState.books.get(book) match {
+      case Some(bookState) =>
+        bookState.copy(nrSold = bookState.nrSold + nrSold)
+      case None => BookStatistics(book, nrSold)
+    }
+    oldState.copy(oldState.sequence + 1,
+      oldState.books + (book -> bookStat))
   }
+
+  def addBooksSold(book: String, nrSold: Int): Unit =
+    stateAgent send (updateBooksSold(book, nrSold) _)
 
   def addBooksSoldAndReturnNewState(book: String,
-                                    nrSold: Int): StateBookStatistics = {
-    val future = stateAgent alter (oldState => {
-      val bookStat = oldState.books.get(book) match {
-        case Some(bookState) =>
-          bookState.copy(nrSold = bookState.nrSold + nrSold)
-        case None => new BookStatistics(book, nrSold)
-      }
-      oldState.copy(oldState.sequence + 1,
-        oldState.books + (book -> bookStat))
-    })
-    Await.result(future, 1 second)
-  }
+                                    nrSold: Int): Future[StateBookStatistics] =
+    stateAgent alter (updateBooksSold(book, nrSold) _)
 
-  def getStateBookStatistics(): StateBookStatistics = {
+  def getStateBookStatistics(): StateBookStatistics =
     stateAgent.get()
-  }
 }
